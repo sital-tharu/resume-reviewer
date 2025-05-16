363c86fb00a660dcc1bfeaeedf8120a8f5a53907
sital-tharu <sitatharu769@gmail.com>
2025-05-16 21:41:29 +0530

    error handaling

--- api/index.py
+++ api/index.py
@@ -4,3 +4,12 @@ app = create_app()
 
 # Required for Vercel
 app.handler = app
+
+try:
+    from app import create_app
+    app = create_app()
+    app.handler = app
+except Exception as e:
+    import sys
+    sys.stderr.write("APP ERROR: " + str(e) + "\n")
+    raise
