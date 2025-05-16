44b9ccc2402a54ac7313520a55cf2b6088c5a3ee
sital-tharu <sitatharu769@gmail.com>
2025-05-16 22:51:55 +0530

    fix error 500

--- api/index.py
+++ api/index.py
@@ -1,27 +1,11 @@
-import sys
-
-sys.stderr.write("API INDEX.PY STARTED\n")
+# index.py
+print("API INDEX.PY STARTED")
 
 try:
     from app import create_app
     app = create_app()
-    app.handler = app  # Required by Vercel for Python apps
+    app.handler = app  # Required by Vercel
 except Exception as e:
+    import sys
     sys.stderr.write("APP ERROR: " + str(e) + "\n")
     raise
-
-# from app import create_app
-
-# app = create_app()
-
-# # Required for Vercel
-# app.handler = app
-
-# try:
-#     from app import create_app
-#     app = create_app()
-#     app.handler = app
-# except Exception as e:
-#     import sys
-#     sys.stderr.write("APP ERROR: " + str(e) + "\n")
-#     raise
