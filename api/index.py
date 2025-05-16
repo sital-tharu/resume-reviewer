b8a72684b6d040b8184d0914397395c337e5c8d4
sital-tharu <sitatharu769@gmail.com>
2025-05-16 23:00:07 +0530

    update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,8 @@
-# index.py
-print("API INDEX.PY STARTED")
+from app import create_app
 
 try:
-    from app import create_app
     app = create_app()
-    app.handler = app  # Required by Vercel
+    app.handler = app  # Required for Vercel
 except Exception as e:
     import sys
     sys.stderr.write("APP ERROR: " + str(e) + "\n")
